1ee2b14d118707855bc8d02eddde489e8d25063c
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-08 01:19:11 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,15 +1,13 @@
 class EmailParser
-
-attr_accessor :emails
+  attr_accessor :emails
 
   def initialize(emails)
     @emails = emails
   end
 
   def parse
-      seperated_emails = @emails.split(/[ ,]/)
-      unique_emails = seperated_emails.uniq
-      unique_emails.reject {|email| email.empty?}
-    end
-
+    emails.split.map do |email|
+      email.split(',')
+    end.flatten.uniq
+  end
 end
